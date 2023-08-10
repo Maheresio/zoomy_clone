c73b4d60a64e1a21c5398f4a892413cf4560406d
Maheresio <zamalek.545r5@gmail.com>
2023-08-11 00:57:46 +0300

    adding on boarding view route

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:zoomy_clone/features/splash/view/splash_view.dart';
 
-
+import '../../features/on_boarding/view/on_boarding_view.dart';
 import '../helpers/custom_transition_page.dart';
 
 abstract class AppRouter {
@@ -16,27 +16,27 @@ abstract class AppRouter {
       GoRoute(
         path: '/',
         builder: (context, state) {
-          return  const SplashView();
+          return const SplashView();
         },
       ),
-      // GoRoute(
-      //   path: kOnBoardingView,
-      //   builder: (context, state) {
-      //     return const OnBoardingView();
-      //   },
-      //   pageBuilder: (context, state) => buildPageWithDefaultTransition<void>(
-      //     state: state,
-      //     child: const OnBoardingView(),
-      //   ),
-      // ),
       GoRoute(
-        path: kHomeView,
+        path: kOnBoardingView,
         builder: (context, state) {
-          return  Container();
+          return const OnBoardingView();
         },
         pageBuilder: (context, state) => buildPageWithDefaultTransition<void>(
           state: state,
-          child:  Container(),
+          child: const OnBoardingView(),
+        ),
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) {
+          return Container();
+        },
+        pageBuilder: (context, state) => buildPageWithDefaultTransition<void>(
+          state: state,
+          child: Container(),
         ),
       ),
       // GoRoute(
