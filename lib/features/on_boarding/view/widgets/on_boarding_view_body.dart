750f74c7926c966e481fda6377cfb3659e6bc7a3
Maheresio <zamalek.545r5@gmail.com>
2023-08-11 00:54:05 +0300

    adding onboarding view

---
+++ lib/features/on_boarding/view/widgets/on_boarding_view_body.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zoomy_clone/core/utils/app_assets.dart';
+import 'package:zoomy_clone/core/utils/app_colors.dart';
+
+class OnBoardingViewBody extends StatelessWidget {
+  const OnBoardingViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Start or join a meeting',
+          style: Theme.of(context).textTheme.titleLarge,
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 30.h,
+        ),
+        Image.asset(AppAssets.onBoardingImgPath),
+        SizedBox(
+          height: 30.h,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              minimumSize: MaterialStateProperty.all(
+                Size(
+                  double.infinity,
+                  45.h,
+                ),
+              ),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.r),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(
+                AppColors.kButtonColor,
+              ),
+            ),
+            child: Text(
+              'Google Sign In',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
