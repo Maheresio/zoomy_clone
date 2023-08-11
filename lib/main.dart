d8595489b7fa3134ce6a5c80d03fa8dea2d68d4d
Maheresio <zamalek.545r5@gmail.com>
2023-08-11 17:47:30 +0300

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:zoomy_clone/core/utils/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'core/helpers/custom_status_bar.dart';
 import 'core/utils/app_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await ScreenUtil.ensureScreenSize();
   customStatusBar();
 
